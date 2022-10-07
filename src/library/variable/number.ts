8c5f94bfa3123b6b11c9045dbeb13b557aed9d42
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2022-10-07 18:11:40 +0300

    Mailer

--- src/library/variable/number.ts
+++ src/library/variable/number.ts
@@ -2,6 +2,7 @@ declare global {
     interface Number {
         isInt(): boolean
         isFloat(): boolean
+        getPercent(min: number, max: number): number
     }
 }
 
@@ -14,6 +15,10 @@ Number.prototype.isFloat = function () {
     if (typeof this !== "number") return false;
     let n  = this;
     return Number(n) === n && n % 1 !== 0;
+}
+Number.prototype.getPercent = function (min, max) {
+    if (typeof this !== "number") return 0;
+    return 100 * (this - min) / (max - min)
 }
 
 export default {}
