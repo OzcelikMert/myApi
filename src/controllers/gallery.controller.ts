6f6a70ffac659d1c112edb60c636658b0cce37e4
Mert ÖZÇELİK <mert.ozcelikk@hotmail.com>
2023-02-09 19:00:47 +0300

    Things to do

--- src/controllers/gallery.controller.ts
+++ src/controllers/gallery.controller.ts
@@ -2,13 +2,22 @@ import {Request, Response} from "express";
 import {DateMask} from "../library/variable"
 import {ErrorCodes, Result, StatusCodes} from "../library/api";
 import {InferType} from "yup";
-import fs from "fs";
+import fs, {Stats} from "fs";
 import {Config} from "../config";
 import path from "path";
 import sharp from "sharp";
 import multer from "multer";
 import gallerySchema from "../schemas/gallery.schema";
 import logMiddleware from "../middlewares/log.middleware";
+
+function getImageResult(name: string, stats: Stats) {
+    return {
+        name: name,
+        sizeKB: Number(stats.size) / 1024,
+        sizeMB: Number(stats.size) / (1024 * 1024),
+        createdAt: stats.ctime,
+    }
+}
 
 export default {
     get: async (
@@ -21,12 +30,13 @@ export default {
             const fileType = [".jpg", ".png", ".webp", ".gif", ".jpeg"];
 
             await new Promise(resolve => {
-                fs.readdir(Config.paths.uploads.images, (err, images) => {
+                fs.readdir(Config.paths.uploads.images, async (err, images) => {
                     for(let i=0; i < images.length; i++) {
                         let image = images[i];
                         if(fs.existsSync(path.resolve(Config.paths.uploads.images, image))) {
                             if (fileType.includes(path.extname(image))){
-                                serviceResult.data.push(image)
+                                let fileStat = fs.statSync(path.resolve(Config.paths.uploads.images, image))
+                                serviceResult.data.push(getImageResult(image, fileStat));
                             }
                         }
                     }
@@ -78,8 +88,14 @@ export default {
                         let data = await sharp(req.file?.buffer, {animated: true})
                             .webp({quality: 80, force: true, loop: 0})
                             .toBuffer();
-                        fs.createWriteStream(path.resolve(Config.paths.uploads.images, ref)).write(data);
-                        serviceResult.data.push(ref);
+
+                        await new Promise(resolveCreate => {
+                            fs.createWriteStream(path.resolve(Config.paths.uploads.images, ref)).write(data, (error) => {
+                                let fileStat = fs.statSync(path.resolve(Config.paths.uploads.images, ref))
+                                serviceResult.data.push(getImageResult(ref, fileStat));
+                                resolveCreate(0);
+                            });
+                        })
                     } catch (e) {
                         serviceResult.status = false;
                         serviceResult.errorCode = ErrorCodes.uploadError;
